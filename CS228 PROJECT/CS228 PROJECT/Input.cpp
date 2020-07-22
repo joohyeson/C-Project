a205f80c287ad2c810cbbcd31eaefdd7bb49887d
d.jeong <jdoeun2@gmail.com>
2020-07-22 11:02:53 +0900

    Remove the non-sgml lib files from the project. Remove the non small libs from the project properties like OpenGL32 etc. Change language to C++17. Change warning level to 4. Change settings to treat warnings as errors. Remove some warnings.

--- CS228 PROJECT/CS228 PROJECT/Input.cpp
+++ CS228 PROJECT/CS228 PROJECT/Input.cpp
@@ -12,16 +12,6 @@ Creation date: 21/07/2020
 #include "Input.h"
 #include "Engine.h" //Engine::GetWindow().GetSize()
 
-std::bitset<sf::Keyboard::KeyCount> Input::mKeyPressed;
-std::bitset<sf::Keyboard::KeyCount> Input::mKeyReleased;
-std::bitset<sf::Keyboard::KeyCount> Input::mKeyTriggered;
-
-std::bitset<sf::Mouse::ButtonCount> Input::mMousePressed;
-std::bitset<sf::Mouse::ButtonCount> Input::mMouseReleased;
-std::bitset<sf::Mouse::ButtonCount> Input::mMouseTriggered;
-
-sf::Vector2i Input::mMouse;
-
 Input::Input()
 {
     mKeyPressed.reset();
@@ -130,8 +120,8 @@ void Input::SetReleasedMouse(sf::Mouse::Button mouseButton)
 
 void Input::SetMousePosition(sf::Vector2i position)
 {
-    mMouse.x = position.x - Engine::GetWindow().GetSize().x / 2.f;
-    mMouse.y = -(position.y - Engine::GetWindow().GetSize().y / 2.f);
+    mMouse.x = position.x - Engine::GetWindow().GetSize().x / 2;
+    mMouse.y = -(position.y - Engine::GetWindow().GetSize().y / 2);
 }
 
 void Input::Reset()
