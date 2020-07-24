d093e2b87e54001d5da10f734c3b49e3a6f0bf6e
joohyeson <juhye.son990902@gmail.com>
2020-07-24 10:10:53 +0900

    Add Colors.h

---
+++ CS228 PROJECT/CS228 PROJECT/Colors.h
@@ -0,0 +1,28 @@
+/*--------------------------------------------------------------
+Copyright (C) 2020 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+File Name: Colors.h
+Purpose: This is Colors header file.
+Project: CS280
+Author: Daeun Jeong, Juhye Son
+Creation date: 21/07/2020
+-----------------------------------------------------------------*/
+
+#pragma once
+
+#define RED 0xFF0000FF
+#define GREEN 0x00695CFF
+#define BLUE 0x0000FFFF
+#define PURPLE 0x673AB7FF
+#define DARK_BLUE 0x1A237EFF
+
+#define BABY_PINK 0xF48FB1FF
+#define LIGHT_PURPLE 0xEA80FCFF
+#define LIGHT_BLUE 0x90CAF9FF
+#define LIGHT_YELLOW 0xFFFF8DFF
+#define OLIVE 0x7CB342FF
+#define LIME 0xC6FF00FF
+
+#define WHITE 0xFFFFFFFF
+#define BLACK 0x000000FF
